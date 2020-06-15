6ce423d6f071514d3ec57393be04818d5df52df9
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-15 10:44:16 -0700

    add places, properties, methods, and prototypes to README.md. begin basic database of keys with constructor and add data for each of 10 cities.
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
-# _{Application Name}_
+# _{Places to go}_
 
-#### _{Brief description of application}, {Date of current version}_
+#### A list of places we've been or to go to, 06-15-2020
 
-#### By _**{List of contributors}**_
+#### By **Kevin Davis**, **Jerrod Styrk**
 
-## Description
+## A list of places we have been or want to go
 
-_{This is a detailed description of your application. Its purpose and usage.  Give as much detail as needed to explain what the application does, and any other information you want users or other developers to have. }_
+This is a list of places we have been and some little facts you may enjoy pertaining to them.
+
+## Places 
+* Boston MA
+* Charleston SC
+* Orlando FL
+* Carlsbad NM
+* Portland OR
+* New York NY
+* Los Angeles CA
+* New Orleans LA
+* Mexico City MX
+* Paris FR
+
+## Place Properties
+* City
+* State/Province
+* Country
+* Landmarks
+* Famous Food
+* Population
+* Common Languages
+
+## Methods
+* Add Location
+* Remove Location
+
+## Prototypes
+* Full Location
 
 ## Setup/Installation Requirements
 
@@ -16,22 +44,22 @@ _{This is a detailed description of your application. Its purpose and usage.  Gi
 * _easy-to-understand_
 * _format_
 
-_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
-
 ## Known Bugs
 
-_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+None at this time
 
 ## Support and contact details
 
-_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+Feel free to contact us with any feedback 
 
 ## Technologies Used
 
-_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+* jQuery
 
 ### License
 
-*{Determine the license under which this application can be used.  See below for more details on licensing.}*
-
-Copyright (c) 2016 **_{List of contributors or company name}_**
+Copyright (c) 2020 **Kevin Davis**, **Jerrod Styrk**
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,48 @@
+// business logic
+
+function PlacesDb() {
+  this.places = []
+}
+
+PlacesDb.prototype.addPlace = function(place)
+  this.places.push(place);
+
+function Place( city, state, country, landmarks, food, pop, lang) {
+  this.city = city;
+  this.state = state;
+  this.country = country;
+  this.landmarks = landmarks;
+  this.food = food;
+  this.pop = pop;
+  this.lang = lang;
+}
+
+Place.prototype.fullLocation = function () {
+  return this.city + " " + this.state + " " + this.country;
+}
+
+let bos = new Place("Boston", "Massachusetts", "United States of America", ["Fort Warren", "Fenway Park", "Boston Harbor"], "Chowda!", 694583, ["English", "Portugese", "Spanish"])
+
+let char = new Place("Charleston", "South Carolina", "United States of America", ["Fort Sumter", "The Citadel", "Charleston City Market"], "Boiled Peanuts", 130113, ["English", "French", "Gullah"])
+
+let orl = new Place("Orlando", "Florida", "United States of America", ["Disney World", "Universal Studios", "Seaworld"], "Conch Fritters", 285713, ["English", "Creole", "Spanish"])
+
+let car = new Place("Carlsbad", "New Mexico", "United States of America", ["Carlsbad cavern", "Sitting Bull Falls", "Living Desert Zoo"], "Burritos", 29331, ["English", "Polish", "Spanish"])
+
+let pdx = new Place("Portland", "Oregon", "United States of America", ["Japanese Garden", "Pittock Mansion", "Multnomah Falls"], "Voodoo Doughnuts", 2151000, ["English", "German", "Russian"])
+
+let nyc = new Place("New York City", "New York", "United States of America", ["Empire State Building", "Central Park", "Natural History Museum"], "Pizza", 8336817, ["English", "Spanish", "Chinese"])
+
+let los = new Place("Los Angeles", "California", "United States of America", ["Hollywood Sign", "Griffith Observatory", "La Brea Tar Pits"], , 3990000, ["English", "Spanish", "Chinese"])
+
+let nola = new Place("New Orleans", "Louisiana", "United States of America", ["French Quarter", "Bourbon Street", "St. Louis Cathedral"], "Jambalaya", 391006, ["English", "Creole", "French"])
+
+let cdmx = new Place("Mexico City", "Mexico City", "Mexico", ["Bosque de Chapultepec", "Palacio de Bella Artes", "Xochimilco"], "Tlacoyos", 21782378, ["Spanish", "Engish", "Nahuatl"])
+
+let paris = new Place("Paris", "‎Île-de-France", "France", ["Eiffel Tower", "Louvre Museum", "Notre Dame Cathedral"], "Baguettes", 2148271, ["French", "Arabic", "German"])
+
+
+// User interface Logic
 $(document).ready(function() {
 event.preventDefault();
 })
